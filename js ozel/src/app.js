a42e190ab6e7c79aaba8474091717ebc92b20011
Adil Abtalıbov <abtalibov11@gmail.com>
2025-04-03 17:56:14 +0400

    js task 2 ci

--- js ozel/src/app.js
+++ js ozel/src/app.js
@@ -1,25 +1,29 @@
-let a=[1,2,3,4,5,6,8];
-let b=[4,5,6,7,8,9];
-let c = ortaqel(a,b);
-function ortaqel(a,b) {
-    return a.filter(k => b.includes(k));
-    
-}
-console.log(c);
-for (let v = 0; v < c.length; v++) {
-    if (v % 2 == 0) {
-    console.log("cutdur")
-} 
-else {
-    console.log("tekdir")
-    }
-}
+// let a=[1,2,3,4,5,6,8,9];
+// let b=[4,5,6,7,8,9];
+// let c = ortaqel();
+// function ortaqel() {
+//     return a.filter(k => b.includes(k));
 
 
-
-// for (let k = 0; k < a.length; k++) {
-//     for (let j = 0; j < b.length; j++) {
-
-        
-//     }
 // }
+// console.log(c);
+
+// for (let v = 0; v < c.length; v++) {
+//     if (c[v] % 2 == 0) {
+//     console.log("cutdur")
+// } 
+// else {
+//     console.log("tekdir")
+//     }
+// }
+
+// Artan sira
+let a = [0, 2, 4, 5, 7, -1, -4, -5, -20, -6, -100, -98, -12];
+let c = []
+for (let i = 0; i < a.length; i++) {
+    if (a[i] < 0) {
+        c.push(a[i])
+    }
+    c.sort((a, b) => b - a)
+}
+console.log (c)
